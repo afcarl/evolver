686394c262a29a9dd6a684f70202c81f792e5aff
jinseokYeom <jinyeom95@gmail.com>
2015-08-05 20:36:12 -0500

    progress:

--- optimization_analysis.py
+++ optimization_analysis.py
@@ -1,3 +1,5 @@
+# import built-in
+import os
 # import algorithm implementation
 from Gradient_Descent import gradient_descent
 from Hill_Climbing_for_minimum import hill_climbing
@@ -9,46 +11,56 @@ from Genetic_Algorithm_for_minimum import genetic_algorithm
 N_TRIAL = 20
 F1 = 0      # f(x) = (6x - 2)^2 * sin(12x - 4)
 F2 = 1      # f(x) = -2x^3 * sin(x^5 + 4)
+F3 = 2      #
 
-
-
-# average performance
-gradient_descent_average = 0.0
-hill_climbing_average = 0.0
-simulated_annealing_average = 0.0
-mu_lambda_evoulution_strategy_average = 0.0
-genetic_algorithm_average = 0.0
-
-# get performance results
-for trial in range(N_TRIAL):
-    print "**** TRIAL " + str(trial + 1) + " ****"
-    gd_x, gd_y = gradient_descent()
-    hc_x, hc_y = hill_climbing()
-    sa_x, sa_y = simulated_annealing()
-    ml_x, ml_y = mu_lambda_evoulution_strategy()
-    ga_x, ga_y = genetic_algorithm()
-
-    # sum up the performance result for each algorithm
-    gradient_descent_average += gd_y
-    hill_climbing_average += hc_y
-    simulated_annealing_average += sa_y
-    mu_lambda_evoulution_strategy_average += ml_y
-    genetic_algorithm_average += ga_y
-
-# calculate average performance
-gradient_descent_average /= N_TRIAL
-hill_climbing_average /= N_TRIAL
-simulated_annealing_average /= N_TRIAL
-mu_lambda_evoulution_strategy_average /= N_TRIAL
-genetic_algorithm_average /= N_TRIAL
+# print test function
+def print_test_function(test_number):
+    if test_number == 0:
+        print "f(x) = (6x - 2)^2 * sin(12x - 4)"
+    elif test_number == 1:
+        print "f(x) = -2x^3 * sin(x^5 + 4)"
 
 # print average performance
-def printAveragePerformance():
+def printAveragePerformance(test_number):
+    # average performance
+    gradient_descent_average = 0.0
+    hill_climbing_average = 0.0
+    simulated_annealing_average = 0.0
+    mu_lambda_evoulution_strategy_average = 0.0
+    genetic_algorithm_average = 0.0
+
+    # get performance results
+    for trial in range(N_TRIAL):
+        print "**** TRIAL " + str(trial + 1) + " ****"
+        gd_x, gd_y = gradient_descent(test_number)
+        hc_x, hc_y = hill_climbing(test_number)
+        sa_x, sa_y = simulated_annealing(test_number)
+        ml_x, ml_y = mu_lambda_evoulution_strategy(test_number)
+        ga_x, ga_y = genetic_algorithm(test_number)
+
+        # sum up the performance result for each algorithm
+        gradient_descent_average += gd_y
+        hill_climbing_average += hc_y
+        simulated_annealing_average += sa_y
+        mu_lambda_evoulution_strategy_average += ml_y
+        genetic_algorithm_average += ga_y
+
+    # calculate average performance
+    gradient_descent_average /= N_TRIAL
+    hill_climbing_average /= N_TRIAL
+    simulated_annealing_average /= N_TRIAL
+    mu_lambda_evoulution_strategy_average /= N_TRIAL
+    genetic_algorithm_average /= N_TRIAL
+
+    # print average performance
+    os.system("clear")
     print "**** Average Performance ****"
-    print "f(x) = (6x - 2)^2 * sin(12x - 4)"
+    print_test_function(test_number)
     print "Ideal solution: -6.02"
     print "gDe: " + str(gradient_descent_average)
     print "hCl: " + str(hill_climbing_average)
     print "sAn: " + str(simulated_annealing_average)
     print "mLa: " + str(mu_lambda_evoulution_strategy_average)
     print "gAl: " + str(genetic_algorithm_average)
+
+printAveragePerformance(0)
